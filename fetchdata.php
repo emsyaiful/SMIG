5ca10e6710614c2edc38f8b58da33b89bd4effd0
uul <jihad.amrulloh@gmail.com>
2015-11-08 11:31:59 +0700

    baru lagi oi

--- fetchdata.php
+++ fetchdata.php
@@ -1,37 +1,54 @@
 <?php
 	include '../include/oracle.php';
 	$dbOra = new database();
+	$defaultParam = array("7000","7403");
 	if($_GET['sel']=="avgcargo"){
-		$sql = "SELECT round((avg(DATE_CVY - TGL_ANTRI)*24*60),2) average, tipe_truk from ZREPORT_RPT_REAL where (SELECT TO_CHAR (SYSDATE-1, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') and com=7000 and plant=7403 GROUP BY TIPE_TRUK";
+		$sql = "SELECT ROUND((avg(DATE_CVY - TGL_ANTRI)*24*60),2) AVERAGE, tipe_truk from ZREPORT_RPT_REAL where (SELECT TO_CHAR (SYSDATE-1, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') AND COM=$defaultParam[0] AND PLANT=$defaultParam[1] GROUP BY TIPE_TRUK";
 		$dbOra->query($sql);
 		$aarSql = $dbOra->build_results($sql);
 		echo json_encode($aarSql);
 	}else if($_GET['sel']=="cntcargo"){
-		$sql = "SELECT COUNT(NO_TRANSAKSI) COUNTER,TIPE_TRUK FROM ZREPORT_RPT_REAL_NON70 WHERE STATUS<=40 and STATUS>=10 and com=7000 and plant=7403 GROUP BY TIPE_TRUK";
+		$sql = "SELECT COUNT(NO_TRANSAKSI) COUNTER,TIPE_TRUK FROM ZREPORT_RPT_REAL_NON70 WHERE STATUS<=40 AND STATUS>=10 AND COM=$defaultParam[0] AND PLANT=$defaultParam[1] GROUP BY TIPE_TRUK";
 		$dbOra->query($sql);
 		$aarSql = $dbOra->build_results($sql);
 		echo json_encode($aarSql);
 	}else if($_GET['sel']=="avgtmbm"){
-		$sql = "SELECT round((avg(TGL_MASUK - DATE_CVY)*24*60),2) average,tipe_truk from ZREPORT_RPT_REAL where (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') and com=7000 and plant=7403 GROUP BY TIPE_TRUK union SELECT round((avg(TGL_MASUK - DATE_CVY)*24*60),2) average,tipe_truk from ZREPORT_RPT_REAL_NON70 where (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') and com=7000 and plant=7403 GROUP BY TIPE_TRUK";
+		$sql = "SELECT ROUND((avg(TGL_MASUK - DATE_CVY)*24*60),2) AVERAGE,tipe_truk from ZREPORT_RPT_REAL where (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') AND COM=$defaultParam[0] AND PLANT=$defaultParam[1] GROUP BY TIPE_TRUK union SELECT ROUND((avg(TGL_MASUK - DATE_CVY)*24*60),2) AVERAGE,tipe_truk from ZREPORT_RPT_REAL_NON70 where (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') AND COM=$defaultParam[0] AND PLANT=$defaultParam[1] GROUP BY TIPE_TRUK";
 		$dbOra->query($sql);
 		$aarSql = $dbOra->build_results($sql);
 		echo json_encode($aarSql);
 	}else if($_GET['sel']=="cnttmbm"){
-		$sql = "SELECT COUNT(NO_TRANSAKSI),TIPE_TRUK FROM ZREPORT_RPT_REAL_NON70 WHERE STATUS=50 GROUP BY TIPE_TRUK";
+		$sql = "SELECT COUNT(NO_TRANSAKSI) COUNTER,TIPE_TRUK FROM ZREPORT_RPT_REAL_NON70 WHERE STATUS=50 AND COM=$defaultParam[0] AND PLANT=$defaultParam[1] GROUP BY TIPE_TRUK";
 		$dbOra->query($sql);
 		$aarSql = $dbOra->build_results($sql);
 		echo json_encode($aarSql);
 	}else if($_GET['sel']=="avgpbrk"){
-		$sql = "SELECT round((avg(TGL_ISI - TGL_M)*24*60),2) average,tipe_truk from ZREPORT_RPT_REAL where (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') and com=7000 and plant=7403 GROUP BY TIPE_TRUK";
+		$sql = "SELECT PABRIK, MATNR, ROUND((avg(TGL_ISI - TGL_MASUK)*24*60),2) AVERAGE FROM(SELECT PABRIK, SUBSTR(MATNR,1,7) MATNR,TGL_ISI,TGL_MASUK, NO_TRANSAKSI FROM zreport_rpt_real REL JOIN ZREPORT_M_CVY_MAT CVY ON REL.LSTEL = CVY.LINE_BOOMER WHERE COM=$defaultParam[0] AND PLANT=$defaultParam[1] AND PABRIK is not null and (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY')) GROUP BY PABRIK, MATNR";
 		$dbOra->query($sql);
 		$aarSql = $dbOra->build_results($sql);
 		echo json_encode($aarSql);
 	}else if($_GET['sel']=="cntpbrk"){
-		$sql = "";
+		$sql = "SELECT PABRIK, LSTEL, MATNR, COUNT(NO_TRANSAKSI) COUNTER FROM(SELECT LSTEL, PABRIK, SUBSTR(MATNR,1,7) MATNR, NO_TRANSAKSI FROM ZREPORT_RPT_REAL REL JOIN ZREPORT_M_CVY_MAT CVY ON REL.LSTEL = CVY.LINE_BOOMER WHERE COM=$defaultParam[0] AND PLANT=$defaultParam[1] AND PABRIK IS NOT NULL AND (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY')) GROUP BY PABRIK, LSTEL, MATNR UNION SELECT PABRIK, LSTEL, MATNR, COUNT(NO_TRANSAKSI) COUNTER FROM(SELECT LSTEL, PABRIK, SUBSTR(MATNR,1,7) MATNR, NO_TRANSAKSI FROM ZREPORT_RPT_REAL_NON70 REL JOIN ZREPORT_M_CVY_MAT CVY ON REL.LSTEL = CVY.LINE_BOOMER WHERE COM=$defaultParam[0] AND PLANT=$defaultParam[1] AND PABRIK IS NOT NULL AND STATUS=50) GROUP BY PABRIK, LSTEL, MATNR";
+		$dbOra->query($sql);
+		$aarSql = $dbOra->build_results($sql);
+		echo json_encode($aarSql);
+	}else if($_GET['sel']=="avgtmbk"){
+		$sql = "SELECT ROUND((avg(TGL_ISI - TGL_ANTRI)*24*60),2) AVERAGE, tipe_truk from ZREPORT_RPT_REAL where (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') AND COM=$defaultParam[0] AND PLANT=$defaultParam[1] GROUP BY TIPE_TRUK";
+		$dbOra->query($sql);
+		$aarSql = $dbOra->build_results($sql);
+		echo json_encode($aarSql);
+	}else if($_GET['sel']=="cnttmbk"){
+		$sql = "SELECT COUNT(NO_TRANSAKSI) COUNTER,TIPE_TRUK FROM ZREPORT_RPT_REAL WHERE (SELECT TO_CHAR (SYSDATE, 'DD-MM-YYYY') \"NOW\" FROM DUAL) = TO_CHAR(TGL_SPJ, 'DD-MM-YYYY') AND COM=$defaultParam[0] AND PLANT=$defaultParam[1] GROUP BY TIPE_TRUK";
+		$dbOra->query($sql);
+		$aarSql = $dbOra->build_results($sql);
+		echo json_encode($aarSql);
+	}else if($_GET['sel']=="test"){
+		$sql = "SELECT LSTEL, PABRIK FROM ZREPORT_RPT_REAL JOIN ZREPORT_M_CVY_MAT ON LSTEL=PABRIK WHERE STATUS=50 AND COM=$defaultParam[0] AND PLANT=$defaultParam[1] AND(ROWNUM <= 10)";
 		$dbOra->query($sql);
 		$aarSql = $dbOra->build_results($sql);
 		echo json_encode($aarSql);
 	}
 	else
 		header("Location: /dev/sd/stmj/scm/");
+
 ?>
--- js/app.js
+++ js/app.js
@@ -3,13 +3,62 @@ var app = angular.module('inplant', []);
 
 
 app.controller('dashboard', function($scope,$location,$http) {
-	$scope.kargo=[];
+	$scope.kargo={};
+	$scope.tMasuk = {};
+	$scope.tKeluar={};
 	$scope.pabrikSemen=[];
 	$scope.pabrikSilo=[];
 	initPabrikSemen();
 	initPabrikSilo();
 	initData();
+	function initPabrik()
+	{
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=cntpbrk").success(function(response){
+			var pbk1bag={}
+			var pbk2bag={}
+			var pbk3bag={}
+			var pbk4bag={}
+			pbk1bag.no=1;
+			pbk2bag.no=2;
+			pbk3bag.no=3;
+			pbk4bag.no=4; 
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					bag.cnt+=parseInt(response[i].COUNTER);
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					bulk.cnt+=parseInt(response[i].COUNTER);
+				}
 
+			}
+			var countCycleBulk= document.getElementById("countCycleBulk");
+			countCycleBulk.innerHTML=bulk.cnt;
+			
+			var countCycleBag= document.getElementById("countCycleBag");
+			countCycleBag.innerHTML=bag.cnt;
+			
+			
+			
+		});
+		for(i=0;i<4;i++){
+			var pabrik={};
+			pabrik.no=i;
+			pabrik.average=5+i*10;
+			pabrik.pack=[];
+			for(j=0;j<8;j++){
+				var pack={}
+				pack.no=j;
+				pack.jumlah=0;
+				pabrik.pack.push(pack)
+				
+			}
+			$scope.pabrikSemen.push(pabrik);
+
+		}
+	}
 	function initPabrikSemen(){
 		for(i=0;i<4;i++){
 			var pabrik={};
@@ -29,6 +78,13 @@ app.controller('dashboard', function($scope,$location,$http) {
 	}
 
 	function initData(){
+		initKargo();
+		initTimbangMasuk();
+		initTimbangKeluar();		
+	}
+
+	function initKargo()
+	{
 		var bag = {}
 		bag.nama="bag"
 		bag.avg=0
@@ -37,7 +93,7 @@ app.controller('dashboard', function($scope,$location,$http) {
 		bulk.nama = "bulk"
 		bulk.avg=0;
 		bulk.cnt=0;
-		var gaugeBag = new JustGage({
+		var gaugeKargoBag= new JustGage({
 			id: "gaugeKargoBag",
 			value: 0,
 			min: 0,
@@ -45,7 +101,7 @@ app.controller('dashboard', function($scope,$location,$http) {
 			title: "Time in cargo",
 			label: "minute"
 		})
-		var gaugeBulk = new JustGage({
+		var gaugeKargoBulk= new JustGage({
 			id: "gaugeKargoBulk",
 			value: 0,
 			min: 0,
@@ -53,40 +109,8 @@ app.controller('dashboard', function($scope,$location,$http) {
 			title: "Time in cargo",
 			label: "minute"
 		})
-		var gaugeBag = new JustGage({
-			id: "gaugeTimbangBag",
-			value: 0,
-			min: 0,
-			max: 500,
-			title: "Time in cargo",
-			label: "minute"
-		})
-		var gaugeBulk = new JustGage({
-			id: "gaugeTimbangBulk",
-			value: 0,
-			min: 0,
-			max: 500,
-			title: "Time in cargo",
-			label: "minute"
-		})
-		var gaugeBag = new JustGage({
-			id: "gaugeCycleBag",
-			value: 0,
-			min: 0,
-			max: 500,
-			title: "Time in cargo",
-			label: "minute"
-		})
-		var gaugeBulk = new JustGage({
-			id: "gaugeCycleBulk",
-			value: 0,
-			min: 0,
-			max: 500,
-			title: "Time in cargo",
-			label: "minute"
-		})
-		bag.gauge=gaugeBag
-		bulk.gauge=gaugeBulk
+		bag.gauge=gaugeKargoBag
+		bulk.gauge=gaugeKargoBulk
 		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=avgcargo").success(function(response){
 			
 			var jmlBag=0;
@@ -97,6 +121,7 @@ app.controller('dashboard', function($scope,$location,$http) {
 				{
 					bag.avg+=parseFloat(response[i].AVERAGE)
 					jmlBag++;
+					
 				}
 				else if(response[i].TIPE_TRUK==308)
 				{
@@ -111,8 +136,8 @@ app.controller('dashboard', function($scope,$location,$http) {
 			bag.gauge.refresh(bag.avg);
 			
 			bulk.gauge.refresh(bulk.avg)
-			$scope.kargo.push(bag)
-			$scope.kargo.push(bulk)
+			$scope.kargo.bag=bag
+			$scope.kargo.bulk=bulk
 			
 		});
 		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=cntcargo").success(function(response){
@@ -127,17 +152,344 @@ app.controller('dashboard', function($scope,$location,$http) {
 				{
 					bulk.cnt+=parseInt(response[i].COUNTER);
 				}
-				var countcargobulk= document.getElementById("countcargobulk");
-				countcargobulk.innerHTML=bulk.cnt;
-				var countcargobag= document.getElementById("countcargobag");
-				countcargobag.innerHTML=bag.cnt;
 
 			}
+			var countcargobulk= document.getElementById("countcargobulk");
+			countcargobulk.innerHTML=bulk.cnt;
+			var countcargobag= document.getElementById("countcargobag");
+			countcargobag.innerHTML=bag.cnt;
 			
-		});	
+		});
+	}
+	function initTimbangMasuk()
+	{
+		var bag = {}
+		bag.nama="bag"
+		bag.avg=0
+		bag.cnt=0;
+		var bulk = {}
+		bulk.nama = "bulk"
+		bulk.avg=0;
+		bulk.cnt=0;
+		var gaugeTimbangBag= new JustGage({
+			id: "gaugeTimbangBag",
+			value: 0,
+			min: 0,
+			max: 500,
+			title: "Time in Timbang Masuk",
+			label: "minute"
+		})
+		var gaugeTimbangBulk= new JustGage({
+			id: "gaugeTimbangBulk",
+			value: 0,
+			min: 0,
+			max: 500,
+			title: "Time in Timbang Masuk",
+			label: "minute"
+		})
+		bag.gauge=gaugeTimbangBag
+		bulk.gauge=gaugeTimbangBulk
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=avgtmbm").success(function(response){
+			
+			var jmlBag=0;
+			var jmlBulk=0
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					bag.avg+=parseFloat(response[i].AVERAGE)
+					jmlBag++;
+					
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					bulk.avg+=parseFloat(response[i].AVERAGE)
+					jmlBulk++;
+				}
+			}
+			/*Rata-rata waktu tipe truk berdasarkan kode*/
+			bag.avg=bag.avg/jmlBag;
+			bulk.avg=bulk.avg/jmlBulk;
+			
+			bag.gauge.refresh(bag.avg);
+			
+			bulk.gauge.refresh(bulk.avg)
+			$scope.tMasuk.bag=bag
+			$scope.tMasuk.bulk=bulk
+			
+		});
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=cnttmbm").success(function(response){
+			
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					bag.cnt+=parseInt(response[i].COUNTER);
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					bulk.cnt+=parseInt(response[i].COUNTER);
+				}
+
+			}
+			var countTimbangBag= document.getElementById("countTimbangBag");
+			countTimbangBag.innerHTML=bag.cnt;
+			
+			var countTimbangBulk= document.getElementById("countTimbangBulk");
+			countTimbangBulk.innerHTML=bulk.cnt;
+			
+		});
+	}
+	
+	function initTimbangKeluar()
+	{
+		var bag = {}
+		bag.nama="bag"
+		bag.avg=0
+		bag.cnt=0;
+		var bulk = {}
+		bulk.nama = "bulk"
+		bulk.avg=0;
+		bulk.cnt=0;
+		var gaugeCycleBag= new JustGage({
+			id: "gaugeCycleBag",
+			value: 0,
+			min: 0,
+			max: 500,
+			title: "Time overall",
+			label: "minute"
+		})
+		var gaugeCycleBulk= new JustGage({
+			id: "gaugeCycleBulk",
+			value: 0,
+			min: 0,
+			max: 500,
+			title: "Time overall",
+			label: "minute"
+		})
+		bag.gauge=gaugeCycleBag
+		bulk.gauge=gaugeCycleBulk
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=avgtmbk").success(function(response){
+			
+			var jmlBag=0;
+			var jmlBulk=0
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					bag.avg+=parseFloat(response[i].AVERAGE)
+					jmlBag++;
+					
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					bulk.avg+=parseFloat(response[i].AVERAGE)
+					jmlBulk++;
+				}
+			}
+			/*Rata-rata waktu tipe truk berdasarkan kode*/
+			bag.avg=bag.avg/jmlBag;
+			bulk.avg=bulk.avg/jmlBulk;
+			
+			bag.gauge.refresh(bag.avg);
+			
+			bulk.gauge.refresh(bulk.avg)
+			$scope.tKeluar.bag=bag;
+			$scope.tKeluar.bulk=bulk;
+			
+		});
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=cnttmbk").success(function(response){
+			
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					bag.cnt+=parseInt(response[i].COUNTER);
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					bulk.cnt+=parseInt(response[i].COUNTER);
+				}
+
+			}
+			var countCycleBulk= document.getElementById("countCycleBulk");
+			countCycleBulk.innerHTML=bulk.cnt;
+			
+			var countCycleBag= document.getElementById("countCycleBag");
+			countCycleBag.innerHTML=bag.cnt;
+			
+			
+			
+		});
+	}	
+	
+	function updateKargo(){
+		
+		
+		
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=avgcargo").success(function(response){
+			$scope.kargo.bag.avg=0;
+			$scope.kargo.bulk.avg=0;
+			var jmlBag=0;
+			var jmlBulk=0
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					$scope.kargo.bag.avg+=parseFloat(response[i].AVERAGE)
+					jmlBag++;
+					
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					$scope.kargo.bulk.avg+=parseFloat(response[i].AVERAGE)
+					jmlBulk++;
+				}
+			}
+			/*Rata-rata waktu tipe truk berdasarkan kode*/
+			$scope.kargo.bag.avg=$scope.kargo.bag.avg/jmlBag;
+			$scope.kargo.bulk.avg=$scope.kargo.bulk.avg/jmlBulk;
+			
+			$scope.kargo.bag.gauge.refresh($scope.kargo.bag.avg);
+			$scope.kargo.bulk.gauge.refresh($scope.kargo.bulk.avg);
+
+			console.log($scope.kargo)
+			
+		});
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=cntcargo").success(function(response){
+			$scope.kargo.bag.cnt=0;
+			$scope.kargo.bulk.cnt=0;
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					$scope.kargo.bag.cnt+=parseInt(response[i].COUNTER);
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					$scope.kargo.bulk.cnt+=parseInt(response[i].COUNTER);
+				}
+
+			}
+			var countcargobulk= document.getElementById("countcargobulk");
+			countcargobulk.innerHTML=$scope.kargo.bulk.cnt;
+			var countcargobag= document.getElementById("countcargobag");
+			countcargobag.innerHTML=$scope.kargo.bag.cnt;
+			
+		});
+	}
+	function updateTimbangMasuk(){
+		
+		
+		
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=avgtmbm").success(function(response){
+			$scope.tMasuk.bag.avg=0;
+			$scope.tMasuk.bulk.avg=0;
+			var jmlBag=0;
+			var jmlBulk=0
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					$scope.tMasuk.bag.avg+=parseFloat(response[i].AVERAGE)
+					jmlBag++;
+					
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					$scope.tMasuk.bulk.avg+=parseFloat(response[i].AVERAGE)
+					jmlBulk++;
+				}
+			}
+			/*Rata-rata waktu tipe truk berdasarkan kode*/
+			$scope.tMasuk.bag.avg=$scope.tMasuk.bag.avg/jmlBag;
+			$scope.tMasuk.bulk.avg=$scope.tMasuk.bulk.avg/jmlBulk;
+			
+			$scope.tMasuk.bag.gauge.refresh($scope.tMasuk.bag.avg);
+			$scope.tMasuk.bulk.gauge.refresh($scope.tMasuk.bulk.avg);
+			
+		});
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=cnttmbm").success(function(response){
+			$scope.tMasuk.bag.cnt=0;
+			$scope.tMasuk.bulk.cnt=0;
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					$scope.tMasuk.bag.cnt+=parseInt(response[i].COUNTER);
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					$scope.tMasuk.bulk.cnt+=parseInt(response[i].COUNTER);
+				}
+
+			}
+			var countTimbangBag= document.getElementById("countTimbangBag");
+			countTimbangBag.innerHTML=$scope.tMasuk.bag.cnt;
+			console.log($scope.tMasuk.bag.cnt)
+			console.log($scope.tMasuk.bulk.cnt)
+			var countTimbangBulk= document.getElementById("countTimbangBulk");
+			countTimbangBulk.innerHTML=$scope.tMasuk.bulk.cnt;
+			
+		});
+	}
+	function updateTimbangKeluar(){
+		
+		
+		
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=avgtmbk").success(function(response){
+			$scope.tKeluar.bag.avg=0;
+			$scope.tKeluar.bulk.avg=0;
+			var jmlBag=0;
+			var jmlBulk=0
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					$scope.tKeluar.bag.avg+=parseFloat(response[i].AVERAGE)
+					jmlBag++;
+					
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					$scope.tKeluar.bulk.avg+=parseFloat(response[i].AVERAGE)
+					jmlBulk++;
+				}
+			}
+			/*Rata-rata waktu tipe truk berdasarkan kode*/
+			$scope.tKeluar.bag.avg=$scope.tKeluar.bag.avg/jmlBag;
+			$scope.tKeluar.bulk.avg=$scope.tKeluar.bulk.avg/jmlBulk;
+			
+			$scope.tMasuk.bag.gauge.refresh($scope.tMasuk.bag.avg);
+			$scope.tMasuk.bulk.gauge.refresh($scope.tMasuk.bulk.avg);
+			
+		});
+		$http.get("/dev/sd/stmj/scm/fetchdata.php?sel=cnttmbk").success(function(response){
+			$scope.tKeluar.bag.cnt=0;
+			$scope.tKeluar.bulk.cnt=0;
+			for(i=0;i<response.length;i++)
+			{
+				if(response[i].TIPE_TRUK>=300 && response[i].TIPE_TRUK<308)
+				{
+					$scope.tKeluar.bag.cnt+=parseInt(response[i].COUNTER);
+				}
+				else if(response[i].TIPE_TRUK==308)
+				{
+					$scope.tKeluar.bulk.cnt+=parseInt(response[i].COUNTER);
+				}
+
+			}
+
+			var countCycleBulk= document.getElementById("countCycleBulk");
+			countCycleBulk.innerHTML=$scope.tKeluar.bulk.cnt;
+			
+			var countCycleBag= document.getElementById("countCycleBag");
+			countCycleBag.innerHTML=$scope.tKeluar.bag.cnt;
+			
+			
+		});
 	}
 
-	
 	function initPabrikSilo(){
 		for(i=0;i<4;i++){
 			var pabrik={};
@@ -207,6 +559,9 @@ app.controller('dashboard', function($scope,$location,$http) {
 				updateCounterPabrik(id,100)
 			}
 		}
+		updateKargo();
+		updateTimbangMasuk();
+		updateTimbangKeluar();
 		
 	}
 	function updateCounterPabrik(id,value) {
